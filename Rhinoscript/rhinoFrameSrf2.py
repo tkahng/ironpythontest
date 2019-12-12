d4169d9e41117f4a2c4f174d2e0494133d23f286
tkahng <tkahng@gmail.com>
2019-12-13 02:01:39 +0900

    a

--- Rhinoscript/rhinoFrameSrf2.py
+++ Rhinoscript/rhinoFrameSrf2.py
@@ -1,48 +1,65 @@
 import rhinoscriptsyntax as rs
 
-# obj = rs.GetObject("Select a srf", rs.filter.surface)
+obj = rs.GetObject("Select a srf", rs.filter.surface)
 
-obj = rs.GetObject("Select object", rs.filter.surface + rs.filter.polysurface)
+# obj = rs.GetObject("Select object", rs.filter.surface + rs.filter.polysurface)
 
-
-interval = rs.GetReal("interval", 1.0)
-
-# if rs.IsSurface(obj):
-#     sd
+interval = rs.GetReal("interval", 1)
 
 domainU = rs.SurfaceDomain(obj, 0)
 domainV = rs.SurfaceDomain(obj, 1)
 
 # interval = 1.2
 # domainU = 3.2
-evalpt = []
+rails = []
+shapes = []
 
-i = 0
+i = 0   
 
-while i < domainU:
-    point = rs.EvaluateSurface(obj, interval, 1)
-    rs.AddPoint(point)
-    # evalpt.append(i)
-    i = i+interval
-
-# print(evalpt)
+while i < domainU[1]:
+    point = rs.EvaluateSurface(obj, i, 0)
+    # rs.AddPoint(point)
+    parameter = rs.SurfaceClosestPoint(obj, point)
+    # rails.append(rs.ExtractIsoCurve( obj, parameter, 1))
+    # crv = rails
     
+    crv = rs.ExtractIsoCurve( obj, parameter, 1)
 
-# point = rs.GetPointOnSurface(obj, "Select location for extraction")
-# parameter = rs.SurfaceClosestPoint(obj, point)
-# rs.ExtractIsoCurve( obj, parameter, 2 )
+
+    rails.append(crv)
+
+    plane = rs.CurvePerpFrame(crv, 0)
+    planept = rs.EvaluatePlane(plane, (0,0))
+    rect = rs.AddRectangle( plane, 0.1, 0.1 )
+    cen = rs.CurveAreaCentroid(rect)
+    vec = planept - cen[0]
+    # rs.MoveObjects(rect, vec)
+    # rs.AddSweep1(crv,rect)
+
+    shapes.append(rs.MoveObjects(rect, vec))
+
+    i = i+interval  
+
+print rails
+print shapes
+
+for j in range(len(rails)):
+    rs.AddSweep1(rails[j],shapes[j])
+
+# for rail in rails:
+#     # rail = rails[i]
+#     plane = rs.CurvePerpFrame(rail, 0)
+#     planept = rs.EvaluatePlane(plane, (0,0))
+#     rect = rs.AddRectangle( plane, 0.1, 0.1 )
+#     cen = rs.CurveAreaCentroid(rect)
+#     vec = planept - cen[0]
+#     rs.MoveObjects(rect, vec)
+#     rs.AddSweep1(rail,rect)
+
+
+
 
 # object_ids = rs.GetObjects("Select objects to delete")
 # if object_ids: rs.DeleteObjects(object_ids)
 
 
-
-
-# import rhinoscriptsyntax as rs
-# plane = rs.WorldXYPlane()
-# plane = rs.RotatePlane(plane, 45.0, [0,0,1])
-# planept = rs.EvaluatePlane(plane, (0,0))
-# rect = rs.AddRectangle( plane, 5.0, 15.0 )
-# cen = rs.CurveAreaCentroid(rect)
-# vec = planept - cen[0]
-# rs.MoveObjects(rect, vec)
