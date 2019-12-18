c5c7e12addbf8e0316ccc5c8a834b14993836ce6
tkahng <tkahng@gmail.com>
2019-12-18 22:58:43 +0900

    a

--- Rhinoscript/rhinoWallFromCrvs.py
+++ Rhinoscript/rhinoWallFromCrvs.py
@@ -1,28 +1,21 @@
 import rhinoscriptsyntax as rs
+
 objs = rs.GetObjects("Pick curves to loft", rs.filter.curve)
 width = rs.GetReal("width", 0.4)
+height = rs.GetReal("height", 3)
 plane = rs.ViewCPlane()
-# if objs: rs.AddLoftSrf(objs)
-# curve = rs.AddLine((5,0,0), (10,0,10))
-# surface = rs.AddSrfPt([(0,0,0), (5,0,0), (5,5,0), (0,5,0)])
-# rs.ExtrudeSurface(surface, curve)
 
 def offsetBothCrvs(crvs, width):
     offsets = [] 
     offsets.append(rs.OffsetCurve( crvs, [0,0,0], width/2))
     offsets.append(rs.OffsetCurve( crvs, [0,0,0], -width/2))
     section = rs.AddLoftSrf(offsets,loft_type=2)
+    if offsets: rs.DeleteObjects(offsets)
     return section
-
-# def returnSelection():
-#     objs = rs.LastCreatedObjects()
-#     if objs:
-#         # Only the last circle will be selected
-#         rs.SelectObjects(objs)
 
 def addRail(obj):
     point1 = rs.EvaluateSurface(obj, 0, 0)
-    vec = rs.CreateVector(0, 0, 4)
+    vec = rs.CreateVector(0, 0, height)
     point2 = rs.CopyObject(point1, vec)
     return rs.AddLine(point1, point2)
 
