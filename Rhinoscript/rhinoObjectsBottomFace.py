19108cdfd394c6a782804de382395a8eaa4fa9b7
tkahng <tkahng@gmail.com>
2020-01-14 21:38:40 +0900

    a

--- Rhinoscript/rhinoObjectsBottomFace.py
+++ Rhinoscript/rhinoObjectsBottomFace.py
@@ -6,10 +6,11 @@ objs = rs.GetObjects("Select polysurface to explode", rs.filter.polysurface, pre
 rs.UnselectObjects(objs)
 
 def bottomFaceBndry(obj):
-    faces = []
     bndry = []
-    if rs.IsPolysurface(obj):
-        faces = rs.ExplodePolysurfaces( obj )
+    # if rs.IsPolysurface(obj):
+    #     faces = rs.ExplodePolysurfaces( obj )
+
+    faces = rs.ExplodePolysurfaces(obj)
 
     for face in faces:
         if rs.IsSurface(face):
@@ -25,12 +26,21 @@ def bottomFaceBndry(obj):
                 bndry.append(face)
             else:
                 rs.DeleteObject(face)
+        else:
+            pass
+    return bndry
     print bndry
 
 bottomFaces = []
 
 for obj in objs:
-    bottomFaceBndry(obj)
+    resultFaces = bottomFaceBndry(obj)
+    for resultFace in resultFaces:
+        bottomFaces.append(resultFace)
+
+rs.SelectObjects(bottomFaces)
+
+# print bottomFaces
 
 
     # for bnd in bndry:
