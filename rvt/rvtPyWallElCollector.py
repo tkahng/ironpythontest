ad5c27403f51e283a8bed25ed894255aab86fffc
tkahng <tkahng@gmail.com>
2020-02-13 21:27:19 +0900

    a

--- rvt/rvtPyWallElCollector.py
+++ rvt/rvtPyWallElCollector.py
@@ -1,24 +1,44 @@
 # coding: utf8
 
-from Autodesk.Revit import DB
+# from Autodesk.Revit import DB
+from pyrevit import revit, DB
 
-
-doc = __revit__.ActiveUIDocument.Document
-
+# doc = __revit__.ActiveUIDocument.Document
+# doc = revit.doc.ActiveUIDocument.Document
 
 # Creating collector instance and collecting all the walls from the model
-wall_collector = DB.FilteredElementCollector(doc)\
-                    .OfCategory(DB.BuiltInCategory.OST_Walls)\
-                    .WhereElementIsNotElementType()
+# wc = DB.FilteredElementCollector(doc)\
+#                     .OfCategory(DB.BuiltInCategory.OST_Walls)\
+#                     .WhereElementIsNotElementType()\
+#                     .ToElements()
 
+wc = DB.FilteredElementCollector(revit.doc)\
+                    .OfCategory(DB.BuiltInCategory.OST_Walls)\
+                    .WhereElementIsNotElementType()\
+                    .ToElements()
+
+# col = [x for x in wall_collector]
+
+# w1 = col[0]
+
+cws = []
+bws = []
+
+# for w in wc:
+#     if w.WallType.Kind.ToString() == 'Curtain':
+#         cws.append(w)
+#     else:
+#         bws.append(w)
+
+cws = [w for w in wc if w.WallType.Kind.ToString() == 'Curtain']
 
 # Iterate over wall and collect Volume data
-total_volume = 0.0
+# total_volume = 0.0
 
-for wall in wall_collector:
-    vol_param = wall.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
-    if vol_param:
-        total_volume = total_volume + vol_param.AsDouble()
+# for wall in wall_collector:
+#     vol_param = wall.Parameter[DB.BuiltInParameter.HOST_VOLUME_COMPUTED]
+#     if vol_param:
+#         total_volume = total_volume + vol_param.AsDouble()
 
 # now that results are collected, print the total
-print("Total Volume is: {}".format(total_volume))
+# print("Total Volume is: {}".format(total_volume))
