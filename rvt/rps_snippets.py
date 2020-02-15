b4da151335f75ec754a73ae0b85c56d9b504f866
tkahng <tkahng@gmail.com>
2020-02-15 22:36:22 +0900

    a

--- rvt/rps_snippets.py
+++ rvt/rps_snippets.py
@@ -24,3 +24,54 @@ def get_section_viewfamily():
     return revit.doc.GetDefaultElementTypeId(
         DB.ElementTypeGroup.ViewTypeSection
         )
+
+# curtainwall dim debug
+
+wall = el
+
+
+
+def isParallel(v1,v2):
+    return v1.CrossProduct(v2).IsAlmostEqualTo(DB.XYZ(0,0,0))
+    
+line = wall.Location.Curve
+lineDir = line.GetEndPoint(1) - line.GetEndPoint(0)
+
+refArray = DB.ReferenceArray()
+
+# doc = DocumentManager.Instance.CurrentDBDocument
+
+options = DB.Options()
+options.ComputeReferences = True
+options.IncludeNonVisibleObjects = True
+
+geoElement = wall.get_Geometry(options)
+
+#get side references
+for obj in geoElement:
+    if isinstance(obj,DB.Solid):
+        for f in obj.Faces:
+            faceNormal = f.FaceNormal
+            if isParallel(faceNormal,lineDir):
+                refArray.Append(f.Reference)
+
+
+
+from pyrevit import revit, DB
+
+def cwrefs(cw):
+
+    refArray = DB.ReferenceArray()
+    faces = []
+    options = DB.Options()
+    options.ComputeReferences = True
+    options.IncludeNonVisibleObjects = True
+
+    geoElement = cw.get_Geometry(options)
+
+    for obj in geoElement:
+        if isinstance(obj,DB.Solid):
+            for f in obj.Faces:
+                refArray.Append(f.Reference)
+    return refArray
+
