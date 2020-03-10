9dc9658ad77f5e51d2df942b3bb1bbf8564c1232
tkahng <tkahng@gmail.com>
2020-03-11 04:05:14 +0900

    a

---
+++ Rhinoscript/srfSetLevel.py
@@ -0,0 +1,38 @@
+import rhinoscriptsyntax as rs
+
+objs = rs.GetObjects('select srfs', rs.filter.surface, preselect=True)
+grade = rs.GetString("toggle grade")
+
+def setGrade(grade):
+    if len(grade) == 0:
+        return False
+    else:
+        return True
+
+def srfPointZ(srf):
+    domainU = rs.SurfaceDomain(srf, 0)
+    domainV = rs.SurfaceDomain(srf, 1)
+    u = domainU[1]/2.0
+    v = domainV[1]/2.0
+    point = rs.EvaluateSurface(srf, u, v)
+    el = round(point.Z, 3)
+    return [srf, el]
+
+def groupByElevation(objs, isUG):
+    pairs = map(srfPointZ, objs)
+    values = set(map(lambda x:x[1], pairs))
+    newpairs = [[y for y in pairs if y[1]==x] for x in values]
+    return sorted(newpairs, key=lambda x:x[0][1], reverse=isUG)
+
+def setLevel(sortedpairs, isUG):
+    for idx, pairs in enumerate(sortedpairs, start=1):
+        if isUG: idx = -idx
+        func = lambda x: rs.SetUserText(x[0], "level", str(idx))
+        map(func, pairs)
+
+def process(objs, grade):
+    isUG = setGrade(grade)
+    groups = groupByElevation(objs, isUG)
+    setLevel(groups, isUG)
+
+process(objs, grade)
