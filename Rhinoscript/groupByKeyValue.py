f0b53c5382e21a2135390f90aa5d551e047eecdf
tkahng <tkahng@gmail.com>
2020-03-12 04:28:54 +0900

    a

--- Rhinoscript/groupByKeyValue.py
+++ Rhinoscript/groupByKeyValue.py
@@ -2,3 +2,25 @@ import rhinoscriptsyntax as rs
 import trkRhinoPy as trp
 
 objs = rs.GetObjects('select objects', preselect=True)
+key = rs.GetString('key')
+# objkeys = map(lambda x: rs.GetUserText(x, key), objs)
+values = map(lambda x: rs.GetUserText(x, key), objs)
+# print len(value)
+# print type(value)
+# print value[0]
+# pairs = zip(objs, values)
+# print pairs
+def func(a,b):
+    return [a, b]
+
+pairs = map(func, objs, values)
+setpairs = set(map(lambda x:x[1], pairs))
+newpairs = [[y[0] for y in pairs if y[1]==x] for x in setpairs]
+# sets = set(map(lambda x:x[1], pairs))
+
+for x in newpairs: print len(x[0])
+for x in newpairs: print type(x[0])
+
+for x in newpairs:
+    group = rs.AddGroup()
+    print rs.AddObjectsToGroup(x, group)
