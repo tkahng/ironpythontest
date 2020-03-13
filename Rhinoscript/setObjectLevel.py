aaecd5c1e231b45b1a2a7454aa3025ebe0748440
tkahng <tkahng@gmail.com>
2020-03-14 02:43:57 +0900

    a

--- Rhinoscript/setObjectLevel.py
+++ Rhinoscript/setObjectLevel.py
@@ -1,84 +1,80 @@
 import rhinoscriptsyntax as rs
+import trkRhinoPy as trp
 
 objs = rs.GetObjects('select objs', rs.filter.surface|rs.filter.curve|rs.filter.point, preselect=True)
 grade = rs.GetString("toggle grade")
 
-def setGrade(grade):
-    if len(grade) == 0:
-        return False
-    else:
-        return True
+# def boolToggle(grade):
+#     if len(grade) == 0:
+#         return False
+#     else:
+#         return True
 
-def srfPointZ(srf):
-    domainU = rs.SurfaceDomain(srf, 0)
-    domainV = rs.SurfaceDomain(srf, 1)
-    u = domainU[1]/2.0
-    v = domainV[1]/2.0
-    point = rs.EvaluateSurface(srf, u, v)
-    el = round(point.Z, 3)
-    return [srf, el]
+# def srfPtZPair(srf):
+#     domainU = rs.SurfaceDomain(srf, 0)
+#     domainV = rs.SurfaceDomain(srf, 1)
+#     u = domainU[1]/2.0
+#     v = domainV[1]/2.0
+#     point = rs.EvaluateSurface(srf, u, v)
+#     el = round(point.Z, 3)
+#     return [srf, el]
 
-def crvPointZ(crv):
-    domain = rs.CurveDomain(crv)
-    t = domain[1]/2.0
-    point = rs.EvaluateCurve(crv, t)
-    el = round(point.Z, 3)
-    return [crv, el]
-
-
-
-def makePair(obj):
-    if rs.IsCurve(obj):
-        return crvPointZ(obj)
-    elif rs.IsSurface(obj):
-        return srfPointZ(obj)
-    elif rs.IsPoint(obj):
-        pt = rs.CreatePoint(obj)
-        return [obj, round(pt.Z, 3)]
-    else:
-        pass
-
-def groupByElevation(objs, isUG):
-    pairs = map(makePair, objs)
-    values = set(map(lambda x:x[1], pairs))
-    newpairs = [[y for y in pairs if y[1]==x] for x in values]
-    return sorted(newpairs, key=lambda x:x[0][1], reverse=isUG)
-
-def setLevel(sortedpairs, isUG):
-    for idx, pairs in enumerate(sortedpairs, start=1):
-        grade = 'AG'
-        if isUG: 
-            idx = -idx
-            grade = 'UG'
-        # func = lambda x: rs.SetUserText(x[0], "level", str(idx))
-        # def func(x):
-        #     rs.SetUserText(x[0], "level", str(idx))
-        #     rs.SetUserText(x[0], "grade", grade)
-        #     rs.SetUserText(x[0], "elevation", str(x[1])) 
-        # def func(x):
-        #     keys = 'level grade elevation'
-        #     keys = keys.split()
-        #     vals = [idx, grade, str(x[1])]
-        #     lvldict = dict(zip(keys, vals))
-        #     rs.SetUserText(x[0], 'lvldict', lvldict)
-        # map(func, pairs)
-        map(lambda x: setLevelforDatum(x, idx, grade), pairs)
+# def crvPtZpair(crv):
+#     domain = rs.CurveDomain(crv)
+#     t = domain[1]/2.0
+#     point = rs.EvaluateCurve(crv, t)
+#     el = round(point.Z, 3)
+#     return [crv, el]
 
 
 
-def setLevelforObj(x, idx):
-    rs.SetUserText(x[0], "level", str(idx))
-    rs.SetUserText(x[0], "grade", grade)    
+# def setObjZPair(obj):
+#     if rs.IsCurve(obj):
+#         return crvPtZpair(obj)
+#     elif rs.IsSurface(obj):
+#         return srfPtZPair(obj)
+#     elif rs.IsPoint(obj):
+#         pt = rs.CreatePoint(obj)
+#         return [obj, round(pt.Z, 3)]
+#     else:
+#         pass
 
-def setLevelforDatum(x, idx, grade):
-    rs.SetUserText(x[0], "level", str(idx))
-    rs.SetUserText(x[0], "grade", grade) 
-    rs.SetUserText(x[0], "elevation", str(x[1]))       
+# def groupByElevation(objs, isUG):
+#     pairs = map(setObjZPair, objs)
+#     values = set(map(lambda x:x[1], pairs))
+#     newpairs = [[y for y in pairs if y[1]==x] for x in values]
+#     return sorted(newpairs, key=lambda x:x[0][1], reverse=isUG)
 
-def process(objs, grade):
-    isUG = setGrade(grade)
+# def setLevel(sortedpairs, isUG, func):
+#     for idx, pairs in enumerate(sortedpairs, start=1):
+#         grade = 'AG'
+#         if isUG: 
+#             idx = -idx
+#             grade = 'UG'
+#         map(lambda x: func(x, idx, grade), pairs)
+
+# def setDictforDatum(x, idx, grade):
+#     keys = 'level grade elevation'
+#     keys = keys.split()
+#     vals = [idx, grade, str(x[1])]
+#     lvldict = dict(zip(keys, vals))
+#     rs.SetUserText(x[0], 'lvldict', lvldict)
+
+# def setLevelforObj(x, idx, grade):
+#     rs.SetUserText(x[0], "level", str(idx))
+#     rs.SetUserText(x[0], "grade", grade)    
+
+# def setLevelforDatum(x, idx, grade):
+#     rs.SetUserText(x[0], "level", str(idx))
+#     rs.SetUserText(x[0], "grade", grade) 
+#     rs.SetUserText(x[0], "elevation", str(x[1]))       
+
+
+
+def process(objs, grade, func):
+    isUG = boolToggle(grade)
     groups = groupByElevation(objs, isUG)
-    setLevel(groups, isUG)
+    setLevel(groups, isUG, func)
 
 if __name__ == '__main__':
-    process(objs, grade)  # Put the a call to the main function in the file.    
+    process(objs, grade, setDictforDatum)  # Put the a call to the main function in the file.    
--- Rhinoscript/trkRhinoPy.py
+++ Rhinoscript/trkRhinoPy.py
@@ -80,3 +80,60 @@ def brepGetZ(obj):
     return round(height, 2)
 
 """Level Tools"""
+
+def srfPtZPair(srf):
+    domainU = rs.SurfaceDomain(srf, 0)
+    domainV = rs.SurfaceDomain(srf, 1)
+    u = domainU[1]/2.0
+    v = domainV[1]/2.0
+    point = rs.EvaluateSurface(srf, u, v)
+    el = round(point.Z, 3)
+    return [srf, el]
+
+def crvPtZpair(crv):
+    domain = rs.CurveDomain(crv)
+    t = domain[1]/2.0
+    point = rs.EvaluateCurve(crv, t)
+    el = round(point.Z, 3)
+    return [crv, el]
+
+def setObjZPair(obj):
+    if rs.IsCurve(obj):
+        return crvPtZpair(obj)
+    elif rs.IsSurface(obj):
+        return srfPtZPair(obj)
+    elif rs.IsPoint(obj):
+        pt = rs.CreatePoint(obj)
+        return [obj, round(pt.Z, 3)]
+    else:
+        pass
+
+def groupByElevation(objs, isUG):
+    pairs = map(setObjZPair, objs)
+    values = set(map(lambda x:x[1], pairs))
+    newpairs = [[y for y in pairs if y[1]==x] for x in values]
+    return sorted(newpairs, key=lambda x:x[0][1], reverse=isUG)
+
+def setLevel(sortedpairs, isUG, func):
+    for idx, pairs in enumerate(sortedpairs, start=1):
+        grade = 'AG'
+        if isUG: 
+            idx = -idx
+            grade = 'UG'
+        map(lambda x: func(x, idx, grade), pairs)
+
+def setDictforDatum(x, idx, grade):
+    keys = 'level grade elevation'
+    keys = keys.split()
+    vals = [idx, grade, str(x[1])]
+    lvldict = dict(zip(keys, vals))
+    rs.SetUserText(x[0], 'lvldict', lvldict)
+
+def setLevelforObj(x, idx, grade):
+    rs.SetUserText(x[0], "level", str(idx))
+    rs.SetUserText(x[0], "grade", grade)    
+
+def setLevelforDatum(x, idx, grade):
+    rs.SetUserText(x[0], "level", str(idx))
+    rs.SetUserText(x[0], "grade", grade) 
+    rs.SetUserText(x[0], "elevation", str(x[1]))
